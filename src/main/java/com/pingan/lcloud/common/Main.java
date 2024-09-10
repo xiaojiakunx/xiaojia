859599ef3158a7506a0aa6ba4868a44adbf31a2c
xiaojiakunx <xiaojiakunx@gmail.com>
2024-09-11 01:11:51 +0800

    原始GBKmaster

---
+++ src/main/java/com/pingan/lcloud/common/Main.java
@@ -0,0 +1,12 @@
+package com.pingan.lcloud.common;
+
+/**
+ * @author: xj
+ * @version: 2024/9/11
+ * @description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("这是原始GBK文件");
+    }
+}
