a3eecaca12e420e760a52e56831a9d9d71087e8a
xiaojiakunx <xiaojiakunx@gmail.com>
2024-09-11 10:26:06 +0800

    1

--- src/main/java/com/pingan/lcloud/common/Main.java
+++ src/main/java/com/pingan/lcloud/common/Main.java
@@ -7,13 +7,13 @@ package com.pingan.lcloud.common;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("����ԭʼGBK�ļ�");
+        System.out.println("这是原始GBK文件");
 
-        System.out.println("����GBK�ļ�����1");
+        System.out.println("这是GBK文件新增1");
 
 
-        System.out.println("����");
+        System.out.println("新增");
 
-        System.out.println("����������2");
+        System.out.println("这是新增的2");
     }
 }
